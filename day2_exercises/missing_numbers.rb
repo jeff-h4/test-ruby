ed2e4c0f334bba632fdba4d06b5646b93c6ed3d0
jeff h <jahuangi@yahoo.ca>
2015-08-12 09:11:18 -0700

    updated missing_numbers.rb with better naming, better randomized input

--- day2_exercises/missing_numbers.rb
+++ day2_exercises/missing_numbers.rb
@@ -1,12 +1,12 @@
 
 def find_double_using_hash(input_arr)
-  my_hash = Hash.new(0)
+  frequency_hash = Hash.new(0)
   input_arr.each do |num|
-    my_hash[num] += 1
+    frequency_hash[num] += 1
   end
-  puts my_hash
+  #puts frequency_hash
   puts ""
-  resultkv = my_hash.sort_by {|k,v| v }[-1]
+  resultkv = frequency_hash.sort_by {|k,v| v }[-1]
   puts "The double integer is #{resultkv[0]}" 
 end
 
@@ -22,7 +22,9 @@ def find_double_no_hash(input_arr)
     end
   end
 end
-my_arr = [1,2,3,4,4,5,6]
+#my_arr = [1,2,3,4,4,5,6]
+my_arr = (1..1000000).to_a
+my_arr << my_arr.sample
 puts "Hash method:"
 find_double_using_hash(my_arr)
 puts "No Hash method:"
