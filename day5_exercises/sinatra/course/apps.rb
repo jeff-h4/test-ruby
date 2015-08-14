a3fbd30b82508c7f9bf4e9df9ddc2c2f0b6237a4
jeff h <jahuangi@yahoo.ca>
2015-08-14 13:54:17 -0700

    adding day5 sinatra material. Start of Sessions

---
+++ day5_exercises/sinatra/course/apps.rb
@@ -0,0 +1,140 @@
+
+
+require "sinatra"
+require "sinatra/reloader" if development?
+require "faker" # for BS
+
+# This allows using the sessions feature in Sinatra
+enable :sessions 
+# From Sinatra FAQ: HTTP Authentication
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+end
+# END From Sinatra FAQ: HTTP Authentication
+#========================================================
+# Configuration
+#========================================================
+before do
+  @title = "BEFORE-FILTER: Company BS"
+end
+#========================================================
+# Routes
+#========================================================
+# Basic Route
+get "/" do  # This is a GET Request from the client
+  route_get_slash
+end
+
+post "/" do
+  route_post_slash
+end
+
+get "/companybs" do
+  route_get_companybs
+end
+post "/companybs" do
+  route_post_companybs
+end
+get "/winner" do
+  route_get_winner
+end
+post "/winner" do
+  route_post_winner
+end
+get "/test_protected" do
+  protected!
+  "you're in"
+end
+#========================================================
+# Route Definitions
+#========================================================
+def route_get_slash
+    @page_title = "Home"
+    @intro = "Welcome Home"
+    @company_slogan = "Not Set"
+    #session[:num_visits] += 1
+    erb :index, {layout: :application}
+end
+
+def route_post_slash
+    @page_title = "Home"
+    @intro = "Welcome Home"
+    erb :index, {layout: :application}
+end
+def route_get_companybs
+    @page_title = "Company BS"
+    @intro = "Let's make some BS"
+    @company_slogan = "Not Set"
+    erb :companybs, {layout: :application}
+end
+
+def route_post_companybs
+    @page_title = "Company BS"
+    @intro = "Starting the BS process"
+    @company_slogan = Faker::Company.catch_phrase
+    erb :companybs, {layout: :application}
+end
+
+
+def route_get_winner
+    @page_title = "Let's pick a winner"
+    @intro = "Who is feeling lucky?"
+    erb :winner, {layout: :application}
+end 
+def route_post_winner
+    @page_title = "We picked a winner"
+    @intro = ""
+    winner = params["name-list"].split(",").sample
+    @winner_string = "The winner is #{winner}"
+    session[:the_last_winner] = winner
+    erb :winner, {layout: :application}
+end 
+#def route_get_personality_test
+#    @page_title = "Personality test (index)"
+#    @intro = "Let's start the Personality test"
+#    #erb :index, {layout: :application}
+#    "Welcome to the Personality Test"
+#end
+#
+#
+#def route_post_personality
+#  # it looks like 'params' hash is available, without having to explicitly pass it
+#  @page_title = "Personality Result Page"
+#  @intro = "Here are the results:"
+#  work_best = params["question1"]
+#  interested_in = params["question2"]
+#  consider_yourself = params["question3"]
+#
+#  if work_best == "with-deadlines" && 
+#      interested_in == "ideas" &&
+#      consider_yourself == "rational"
+#    @type = "Rational"
+#  else
+#    @type = "I don't know"
+#  end
+#  #"#{@type}"
+#  erb :personality_result, {layout: :application}
+#end
+
+
+#    # post <URL>
+#    # This is where index.erb's form action must go
+#    post "/sinatra_temperature" do
+#      # params is a Hash object that is given to us by Sinatra
+#      # It contains the key / value pairs from the parameters 
+#      # received from the client.
+#      # If this is a web form, the key matches the "name" HTML attribute,
+#      # and the value will be whatever the uer enters in the input field
+#      temp_deg = params[:temp_deg]
+#      temp_fahr = temp_deg.to_i*9/5.0 + 32
+#      "The temperature in fahrenheit is #{temp_fahr.to_s} degrees"
+#    end
